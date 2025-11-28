1a8819191fa3313a64f822e7f7c8e50ce95284bd
unknown <ndegryse20@gmail.com>
2025-11-28 02:21:36 -0600

    installed jupyter.notebook datasets, variables, and exploratory data analysis

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import pandas as pd
 #merged ratings and movie_titles dataframes on movie_id to create total_data dataframe
 ratings = pd.read_csv("ml-100k/u.data", delimiter="\t", names=["user_id", "movie_id", "rating", "timestamp"])
 movie_titles = pd.read_csv("ml-100k/u.item", delimiter="|", names=["movie_id", "title"], usecols=[0,1], encoding='latin-1')
-total_data = ratings.merge(movie_titles, on ="movie_id")
+total_data = ratings.merge(movie_titles, on="movie_id")
 
 #created a matrix with user_id as index, title as columns and rating as values
 user_movie_matrix = total_data.pivot_table(index="user_id", columns="title", values="rating")
-
-# print(user_movie_matrix)
 
 #converted matrix to csv file
 user_movie_matrix.to_csv("user_movie_matrix.csv")
@@ -18,8 +16,7 @@ user_movie_matrix.to_csv("user_movie_matrix.csv")
 user_movie_matrix_filled = user_movie_matrix.fillna(0)
 user_movie_matrix_filled.to_csv("user_movie_matrix_filled.csv") 
 
-# print(user_movie_matrix_filled)
-
+#raw data layout analysis
 n_users = len(ratings.user_id.unique())
 print(f"Number of unique users in the dataset: {n_users}")
 n_items = len(ratings.movie_id.unique())
@@ -28,3 +25,4 @@ print("-------" * 5)
 print(f"The full rating matrix will have: {n_users * n_items} elements")
 print(f"The number of ratings in the dataset is: {len(ratings)}")
 print(f"The sparsity of the user-movie matrrix is: {len(ratings) / (n_users * n_items) * 100:.2f}%")
+print("-------" * 5)
